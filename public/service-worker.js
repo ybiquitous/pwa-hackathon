33bb195c16d51b6a14ea6e06d55b23dd61224c71
ybiquitous <10koba01@gmail.com>
2017-03-16 16:14:15 +0900

    Make app offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,25 @@
 const cacheName = 'pwa-hackathon'
-const filesToCache = []
+const filesToCache = [
+  '/',
+  '/?homescreen=1',
+  '/index.html',
+  '/index.html?homescreen=1',
+  '/images/icon.png',
+]
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] Install')
-  e.waitUntil(
+  event.waitUntil(
     caches.open(cacheName).then((cache) => {
       console.log('[ServiceWorker] Caching app shell')
       return cache.addAll(filesToCache)
     })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log('[ServiceWorker] fetch')
+  event.respondWith(
+    caches.match(event.request)
   )
 })
